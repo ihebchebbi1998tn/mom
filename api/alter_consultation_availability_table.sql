1c777cc39ebabe9b876b487a4526bd83c4d86361
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2025-10-10 14:56:26 +0100

    de

--- api/alter_consultation_availability_table.sql
+++
@@ -1,3 +0,0 @@
--- Add max_reservations column to consultation_availability table
-ALTER TABLE `consultation_availability` 
-ADD COLUMN `max_reservations` int(11) NOT NULL DEFAULT 3 AFTER `status`;
--- src/lib/uploadthing.ts
+++
@@ -1,8 +0,0 @@
-import { generateUploadButton, generateUploadDropzone, generateReactHelpers } from "@uploadthing/react";
- 
-import type { OurFileRouter } from "./uploadthing-config";
- 
-export const UploadButton = generateUploadButton<OurFileRouter>();
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
- 
-export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
