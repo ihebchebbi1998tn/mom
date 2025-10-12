915f52c7a6a97b95ea4e1a0a2e4cb92206026599
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2025-10-12 16:03:08 +0100

    de

--- api/alter_requests_table_add_recu.sql
+++
@@ -1,6 +0,0 @@
--- Add recu_link column to mom_requests table for receipt upload
-ALTER TABLE mom_requests 
-ADD COLUMN recu_link VARCHAR(500) NULL AFTER admin_notes;
-
--- Add index for better performance when filtering by receipt status
-CREATE INDEX idx_requests_recu_link ON mom_requests(recu_link);
